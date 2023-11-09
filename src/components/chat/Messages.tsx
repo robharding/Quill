531d13fae2d051dff231de13336ebde6080e5135
Rob Harding <robbie.harding@hotmail.com>
2023-11-09 18:18:23 -0500

    Change chat loading from skeleton to spinner

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -81,11 +81,9 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
             );
         })
       ) : isLoading ? (
-        <div className="w-full flex flex-col gap-2">
-          <Skeleton className="h-16" />
-          <Skeleton className="h-16" />
-          <Skeleton className="h-16" />
-          <Skeleton className="h-16" />
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
+          <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
+          <h3 className="font-semibold text-xl">Loading chat...</h3>
         </div>
       ) : (
         <div className="flex-1 flex flex-col items-center justify-center gap-2">
