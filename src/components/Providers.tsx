680d171f6c39e72d22da6ca6466a654a628c2521
Rob Harding <robbie.harding@hotmail.com>
2023-11-09 15:21:46 -0500

    Fix trpc httpbatchlink

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,6 +4,7 @@ import { FC, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/react-query";
+import { absoluteUrl } from "@/lib/utils";
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -15,7 +16,7 @@ const Providers: FC<ProvidersProps> = ({ children }) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: absoluteUrl("/api/trpc"),
         }),
       ],
     })
