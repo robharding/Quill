7828fe9e5b0355a7d17d91bf5f3b823d49833a38
Rob Harding <robbie.harding@hotmail.com>
2023-11-10 19:23:49 -0500

    construct metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,41 @@ export function absoluteUrl(path: string) {
     ? process.env.NEXT_PUBLIC_APP_URL
     : `http://localhost:3000`;
   return `${baseUrl}${path}`;
+}
+
+export function constructMetadata({
+  title = "Quill",
+  description = "Chat with your PDFs using the power of AI",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    icons,
+    metadataBase: new URL("https://quill-steel.vercel.app"),
+    themeColor: "#ffffff",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
