60a2dfbfe48273e5ae1e38e87b84c12fe63bca60
Rob Harding <robbie.harding@hotmail.com>
2023-11-10 19:28:46 -0500

    remove stoppropagation from uploadbutton

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -86,10 +86,7 @@ const UploadDropzone: FC = () => {
             {...getRootProps()}
             className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
           >
-            <div
-              className="flex items-center justify-center h-full w-full"
-              onClick={(e) => e.stopPropagation()}
-            >
+            <div className="flex items-center justify-center h-full w-full">
               <label
                 htmlFor="dropzone-file"
                 className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
