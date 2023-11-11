987cfdf82cdc87161d9e130c73c5e2bc7c36b060
Rob Harding <robbie.harding@hotmail.com>
2023-11-10 19:40:02 -0500

    Try removing middleware

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -86,7 +86,10 @@ const UploadDropzone: FC = () => {
             {...getRootProps()}
             className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
           >
-            <div className="flex items-center justify-center h-full w-full">
+            <div
+              className="flex items-center justify-center h-full w-full"
+              onClick={(e) => e.stopPropagation()}
+            >
               <label
                 htmlFor="dropzone-file"
                 className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
