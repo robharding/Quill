696c104da900be25f31e7dd08fb91c4506bdd4c2
Rob Harding <robbie.harding@hotmail.com>
2023-11-14 17:06:28 -0500

    Show uploading limit for plan on dropzone

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -104,7 +104,9 @@ const UploadDropzone: FC<UploadButtonProps> = ({ isSubscribed }) => {
                     <span className="font-semibold">Click to upload</span> or
                     drag and drop.
                   </p>
-                  <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+                  <p className="text-xs text-zinc-500">
+                    PDF (up to {isSubscribed ? "16" : "4"}MB)
+                  </p>
                 </div>
 
                 {acceptedFiles && acceptedFiles[0] ? (
