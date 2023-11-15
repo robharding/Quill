409b8f34ea3d6b55cb54d0e7d5028ebb64caf861
Rob Harding <robbie.harding@hotmail.com>
2023-11-15 09:13:54 -0500

    unused await

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -15,7 +15,7 @@ export const POST = async (req: NextRequest) => {
   const body = await req.json();
 
   const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  const user = getUser();
   const { id: userId } = user;
   if (!userId) {
     return new Response("Unauthorized", { status: 401 });
