c93e936de1ac6ecb84711034c9a21e24a2e097b5
Rob Harding <robbie.harding@hotmail.com>
2024-02-20 19:45:44 -0500

    Move logout server action to logout route

--- src/app/(auth)/logout/route.ts
+++ src/app/(auth)/logout/route.ts
@@ -1,8 +1,35 @@
-import { logout } from "@/lib/auth";
+import { lucia, validateRequest } from "@/lib/auth";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export function GET() {
   logout();
 
   return redirect("/");
+}
+
+export async function logout(): Promise<ActionResult> {
+  "use server";
+  const { session } = await validateRequest();
+
+  if (!session) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  await lucia.validateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  return { error: null };
+}
+
+interface ActionResult {
+  error: string | null;
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -81,29 +81,3 @@ export const validateRequest = cache(
     return result;
   }
 );
-
-export async function logout(): Promise<ActionResult> {
-  "use server";
-  const { session } = await validateRequest();
-
-  if (!session) {
-    return {
-      error: "Unauthorized",
-    };
-  }
-
-  await lucia.validateSession(session.id);
-
-  const sessionCookie = lucia.createBlankSessionCookie();
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes
-  );
-
-  return { error: null };
-}
-
-interface ActionResult {
-  error: string | null;
-}
