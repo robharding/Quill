4d30eb00c54b28b756da69f9851d0c3b0c506fce
Rob Harding <robbie.harding@hotmail.com>
2024-02-21 08:49:34 -0500

    Removing umami events from nav buttons fixes modals

--- src/components/auth/NavAuthButtons.tsx
+++ src/components/auth/NavAuthButtons.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { FC } from "react";
@@ -11,21 +9,19 @@ const NavAuthButtons: FC<NavAuthButtonsProps> = ({}) => {
   return (
     <>
       <Link
-        href="/login"
+        href="login"
         className={buttonVariants({
           variant: "ghost",
           size: "sm",
         })}
-        data-umami-event="nav-sign-in-button-clicked"
       >
         Sign in
       </Link>
       <Link
-        href="/signup"
+        href="signup"
         className={buttonVariants({
           size: "sm",
         })}
-        data-umami-event="nav-get-started-button-clicked"
       >
         Get started <ArrowRight className="ml-1.5 h-5 w-5" />
       </Link>
